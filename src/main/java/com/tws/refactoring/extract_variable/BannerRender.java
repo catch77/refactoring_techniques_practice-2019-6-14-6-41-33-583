23569aee4b0845920e9ee2ff889d617f2145c46c
catch77 <1297056296@qq.com>
2019-07-15 18:03:44 +0800

    Refactor: BannerRender method

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,17 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
+        if (isMAC(platform) && isIE(browser)) {
             return "IE on Mac?";
         }
         return "banner";
+    }
+
+    private boolean isMAC(String platform) {
+        return platform.toUpperCase().contains("MAC");
+    }
+
+    private boolean isIE(String browser) {
+        return browser.toUpperCase().contains("IE");
     }
 }
--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -1,13 +1,26 @@
 package com.tws.refactoring.extract_variable;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BannerRenderTest {
 
-//    @Test
-//    public void should_return_true_given_MAC_and_IE() {
-//        assertEquals("MAC");
-//    }
+    private static BannerRender bannerRender;
+
+    @Before
+    public void init() {
+        bannerRender = new BannerRender();
+    }
+
+    @Test
+    public void should_return_true_given_MAC_and_IE() {
+        assertEquals("IE on Mac?", bannerRender.renderBanner("mac", "ie"));
+    }
+
+    @Test
+    public void should_return_false_given_MAC_and_IE() {
+        assertEquals("banner", bannerRender.renderBanner("windows", "cho"));
+    }
 }
